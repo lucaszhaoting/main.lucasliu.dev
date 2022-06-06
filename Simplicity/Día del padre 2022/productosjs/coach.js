64a7e087c2d662f1ef3f7bd54ff47fa5e684cf88
lucaszhaoting <51959189+lucaszhaoting@users.noreply.github.com>
2022-06-06 15:44:13 -0300

    Create coach.js

---
+++ Simplicity/Día del padre 2022/productosjs/coach.js
@@ -0,0 +1,90 @@
+[
+{
+"Perfil de papá": "Coach",
+"descripcion": "Precio Original $15810 - Descuento 20% - Precio Final $12648 En 6 cuotas sin interes de $2108",
+"producto": "Perfume Calvin Klein Eternity",
+"foto": "https://main.lucasliu.dev/Simplicity/D%C3%ADa%20del%20padre%202022/fotos/v2/Perfume%20Calvin%20Klein%20Eternity.png",
+"Rango precio": "Menos de $20000",
+"Orden": 11
+},
+{
+"Perfil de papá": "Coach",
+"descripcion": "Precio Original $14900 - Descuento 20% - Precio Final $11920 En 6 cuotas sin interes de $1986,67",
+"producto": "Perfume Lacoste Matchpoint",
+"foto": "https://main.lucasliu.dev/Simplicity/D%C3%ADa%20del%20padre%202022/fotos/v2/Perfume%20Lacoste%20Matchpoint.png",
+"Rango precio": "Menos de $20000",
+"Orden": 10
+},
+{
+"Perfil de papá": "Coach",
+"descripcion": "Precio Original $5722 - Descuento 25% - Precio Final $4291,5 En 6 cuotas sin interes de $715,25",
+"producto": "Afeitadora Sport Gama",
+"foto": "https://main.lucasliu.dev/Simplicity/D%C3%ADa%20del%20padre%202022/fotos/v2/Afeitadora%20Sport%20Gama.png",
+"Rango precio": "Menos de $10000",
+"Orden": 9
+},
+{
+"Perfil de papá": "Coach",
+"descripcion": "Precio Original $4695 - Descuento 10% - Precio Final $4225,5 En 6 cuotas sin interes de $704,25",
+"producto": "Perfume Antonio Banderas Golden Secret",
+"foto": "https://main.lucasliu.dev/Simplicity/D%C3%ADa%20del%20padre%202022/fotos/v2/Perfume%20Antonio%20Banderas%20Golden%20Secret.png",
+"Rango precio": "Menos de $10000",
+"Orden": 8
+},
+{
+"Perfil de papá": "Coach",
+"descripcion": "Precio $1232,5",
+"producto": "Gel Limpieza Facial Dermaglos",
+"foto": "https://main.lucasliu.dev/Simplicity/D%C3%ADa%20del%20padre%202022/fotos/v2/Gel%20Limpieza%20Facial%20Dermaglos.png",
+"Rango precio": "Menos de $4000",
+"Orden": 7
+},
+{
+"Perfil de papá": "Coach",
+"descripcion": "Precio $1199",
+"producto": "Set Pizarra y Fibra Simplicity",
+"foto": "https://main.lucasliu.dev/Simplicity/D%C3%ADa%20del%20padre%202022/fotos/v2/Set%20Pizarra%20y%20Fibra%20Simplicity.png",
+"Rango precio": "Menos de $4000",
+"Orden": 6
+},
+{
+"Perfil de papá": "Coach",
+"descripcion": "Precio Original $999 - Descuento 30% - Precio Final $699,3",
+"producto": "Difusor Blackmoon",
+"foto": "https://main.lucasliu.dev/Simplicity/D%C3%ADa%20del%20padre%202022/fotos/v2/Difusor%20Blackmoon.png",
+"Rango precio": "Menos de $1000",
+"Orden": 5
+},
+{
+"Perfil de papá": "Coach",
+"descripcion": "Precio Original $659 - Descuento 20% - Precio Final $527,2",
+"producto": "Cuaderno Tapa Dura Simplicity",
+"foto": "https://main.lucasliu.dev/Simplicity/D%C3%ADa%20del%20padre%202022/fotos/v2/Cuaderno%20Tapa%20Dura%20Simplicity.png",
+"Rango precio": "Menos de $1000",
+"Orden": 4
+},
+{
+"Perfil de papá": "Coach",
+"descripcion": "Precio $507,5",
+"producto": "Taza de Vidrio para Café",
+"foto": "https://main.lucasliu.dev/Simplicity/D%C3%ADa%20del%20padre%202022/fotos/v2/Taza%20de%20Vidrio%20para%20Café.png",
+"Rango precio": "Menos de $1000",
+"Orden": 3
+},
+{
+"Perfil de papá": "Coach",
+"descripcion": "Precio Original $369 - Descuento 20% - Precio Final $295,2",
+"producto": "Set de Lápices Negros Simplicity",
+"foto": "https://main.lucasliu.dev/Simplicity/D%C3%ADa%20del%20padre%202022/fotos/v2/Set%20de%20Lápices%20Negros%20Simplicity.png",
+"Rango precio": "Menos de $1000",
+"Orden": 2
+},
+{
+"Perfil de papá": "Coach",
+"descripcion": "Precio $323",
+"producto": "Vela Coco&Vainilla Organic Spa",
+"foto": "https://main.lucasliu.dev/Simplicity/D%C3%ADa%20del%20padre%202022/fotos/v2/Vela%20Coco&Vainilla%20Organic%20Spa.png",
+"Rango precio": "Menos de $1000",
+"Orden": 1
+}
+]
