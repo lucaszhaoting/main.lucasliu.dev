d3a0eace8287cc94741e39f80784a86cdd19ab71
lucaszhaoting <51959189+lucaszhaoting@users.noreply.github.com>
2022-06-06 10:55:54 -0300

    Sofia Cozzo added

--- TakeAndEatBot/nombremain.js
+++ TakeAndEatBot/nombremain.js
@@ -1,2 +1,2 @@
 ["Azul Cornejo","Brian Bazan","Camilo Bilbao","Candela Ramos","Carolina Marti","Cecilia Capone","Diego Calio","Federico Monasterio","Ines Milani","Jimena Chaia","Julio Quevedo","Lucas Liu","Luciano Sisto","Matias Sole","Mercedes Cortada","Milagros Mut","Naomi Lucero","Paz Pereyra","Sabina Roca","Santiago Isola","Valentina Han","Victoria de Luca","Victoria Guarch"
-,"Lucas Prueba","Lucas Prueba 2","Lucas Prueba 3","Lucas Prueba 4","Maria Paganini","Alejo Monasterio","Valentin Saravia","Lucas Prueba 5","Stefania Villella"]
+,"Lucas Prueba","Lucas Prueba 2","Lucas Prueba 3","Lucas Prueba 4","Maria Paganini","Alejo Monasterio","Valentin Saravia","Lucas Prueba 5","Stefania Villella","Sofia Cozzo"]
