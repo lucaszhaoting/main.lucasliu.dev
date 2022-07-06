a96e760bde7a89767592a75b1d8e23a4f58de037
lucaszhaoting <51959189+lucaszhaoting@users.noreply.github.com>
2022-07-06 13:02:15 -0300

    Update nombremain.js

--- TakeAndEatBot/nombremain.js
+++ TakeAndEatBot/nombremain.js
@@ -1,3 +1,3 @@
-["Azul Cornejo","Brian Bazan","Camilo Bilbao","Candela Ramos","Cecilia Capone","Diego Calio","Federico Monasterio","Julio Quevedo","Lucas Liu","Luciano Sisto","Matias Sole","Mercedes Cortada","Milagros Mut","Naomi Lucero","Paz Pereyra","Sabina Roca","Santiago Isola","Valentina Han","Victoria Guarch"
+["Azul Cornejo","Brian Bazan","Camilo Bilbao","Candela Ramos","Cecilia Capone","Diego Calio","Federico Monasterio","Julio Quevedo","Lucas Liu","Luciano Sisto","Matias Sole","Milagros Mut","Naomi Lucero","Paz Pereyra","Sabina Roca","Santiago Isola","Valentina Han","Victoria Guarch"
 ,"Maria Paganini","Alejo Monasterio","Valentin Saravia","Stefania Villella","Sofia Cozzo"
 ,"Facundo García","Jana Carta","Soledad Perez"]
